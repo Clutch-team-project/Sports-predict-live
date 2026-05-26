790ca734d2466518bfefb704b4e02c2e6e80c40f
meongkune <meongkune@naver.com>
2026-05-26 19:48:00 +0900

    0527

---
+++ src/test/java/com/example/edu/sports_predict_live/SportsPredictLiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.edu.sports_predict_live;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsPredictLiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
