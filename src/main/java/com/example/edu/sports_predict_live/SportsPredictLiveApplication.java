3cc163ed1c25d23fd64b1f9fae3547e244f73be2
meongkune <meongkune@naver.com>
2026-05-26 19:48:00 +0900

    올림

---
+++ src/main/java/com/example/edu/sports_predict_live/SportsPredictLiveApplication.java
@@ -0,0 +1,13 @@
+package com.example.edu.sports_predict_live;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsPredictLiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportsPredictLiveApplication.class, args);
+    }
+
+}
