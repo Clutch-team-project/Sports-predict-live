e8c860c6ca31163343e3f8908ede16c792fddc34
pigugong102 <a56455946@gmail.com>
2026-06-11 20:22:35 +0900

    board develop으로 머지

--- src/main/java/com/example/edu/sports_predict_live/board/repository/search/BoardSearch.java
+++ src/main/java/com/example/edu/sports_predict_live/board/repository/search/BoardSearch.java
@@ -7,4 +7,4 @@ import org.springframework.data.domain.Pageable;
 public interface BoardSearch {
     // 댓글 개수, 이미지, 카테고리, 글정렬용 view, like 등 받는 sort 포함검색
     Page<BoardListAllDTO> searchWithAll(String[] types, String keyword, String sort, String category, Pageable pageable);
-}
+}//
